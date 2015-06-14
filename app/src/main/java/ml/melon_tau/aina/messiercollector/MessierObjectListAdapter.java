a40048427c626b942780efa499c427c961da25ff
Aina Soler <ainasoga@gmail.com>
2015-06-14 23:40:18 +0200

    All the data in the app

---
+++ app/src/main/java/ml/melon_tau/aina/messiercollector/MessierObjectListAdapter.java
@@ -0,0 +1,38 @@
+package ml.melon_tau.aina.messiercollector;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.TextView;
+
+/**
+ * Created by aina on 14/06/15.
+ */
+public class MessierObjectListAdapter extends CursorAdapter {
+
+    private LayoutInflater mInflater;
+
+    public MessierObjectListAdapter(Context context, Cursor c, int flags) {
+        super(context, c, flags);
+        mInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+        TextView code_content = (TextView) view.findViewById(R.id.Object_Code);
+        code_content.setText(cursor.getString(cursor.getColumnIndex(MessierObjectDbHelper.COLUMN_OBJECT_CODE)));
+        TextView name_content = (TextView) view.findViewById(R.id.Object_Name);
+        name_content.setText(cursor.getString(cursor.getColumnIndex(MessierObjectDbHelper.COLUMN_OBJECT_NAME)));
+        TextView desc_content = (TextView) view.findViewById(R.id.Object_Name);
+        desc_content.setText(cursor.getString(cursor.getColumnIndex(MessierObjectDbHelper.COLUMN_OBJECT_DESC)));
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        return mInflater.inflate(R.layout.messier_object, parent, false);
+    }
+
+}
